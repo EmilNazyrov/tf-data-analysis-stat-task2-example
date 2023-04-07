d4335a4fcb33e033e59bb889f06c8d0eb91fa88a
Emil Nazyrov <72194705+EmilNazyrov@users.noreply.github.com>
2023-04-08 00:04:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -16,6 +16,6 @@ def solution(p: float, x: np.array) -> tuple:
     q2 = chi2.ppf(alpha / 2, n * 2)
     #  loc = x.mean()
     #  scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    l = np.sqrt(sum(x**2) / (q1 * 44))
-    r = np.sqrt(sum(x**2) / (q2 * 44))
+    l = np.sqrt(sum(x**2) / (q1 * 80))
+    r = np.sqrt(sum(x**2) / (q2 * 80))
     return (l, r)
